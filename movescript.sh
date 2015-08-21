ac8560e763aaa7da5aa2a13be4bf4ee8b9a2a50a
Andrew Hilts <andrewchilts@gmail.com>
2015-08-21 11:05:22 -0400

    movescript hostname

--- movescript.sh
+++ movescript.sh
@@ -2,7 +2,7 @@
 ssh user@hostname "cd /pcaps; mkdir $1"
 scp $2 user@hostname:/pcaps/$1/
 if [ "$?" = "0" ]; then
-	echo "Backed up file $2 to cyberchilts:/pcaps/"$1"/"
+	echo "Backed up file $2 to hostname:/pcaps/"$1"/"
 	rm  $2
 	exit 0
 else
